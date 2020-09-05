77d98bf014646c0d4605a3eedd8c01d55a9da37a
Dellvin <dellvin.black@gmail.com>
2020-09-05 02:38:47 -0700

    delete old proj

--- project/include/IZ2_STATIC.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <stdint.h>
-
-int64_t *sumEven(const int64_t *arr, size_t size);
-int64_t *sumNotEven(const int64_t *arr, size_t size);
--- project/include/libIZ2_dymamic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define _GNU_SOURCE 1
-#include <stdio.h>
-#include <stdint.h>
-
-int64_t *sumEvenThread(int64_t *arr, size_t size);
-int64_t *sumNotEvenThread(int64_t *arr, size_t size);
